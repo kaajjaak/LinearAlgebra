6ea1d5cfc05df9803583bfebdee09f166f2f3ed8
akina <yolobroer202@gmail.com>
2023-10-09 20:05:06 +0200

    Added decryption

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 | Project | Topic | Description |
 | ------- | ----- | ------ |
 | [VecDrawer](visualise/VecDrawer.jl) | Vectors in 2D and 3D subspaces | A simple Vector visualiser for planes and spaces |
-| [Cryptography](cryptography/multiplication.jl) | Matrix Multiplication | A simple string encryption tool using Vector Multiplication |
+| [Cryptography](cryptography/multiplication.jl) | Matrix Multiplication & Inverse | A simple string encryption and decryption tool using Matrix Multiplication & Inverse |
 | [Cube](cube/cube.jl) | Vector Addition, Rotation & Transformation | A gif of a rotating cube |
--- cryptography/multiplication.jl
+++ cryptography/multiplication.jl
@@ -1,15 +1,25 @@
 module multiplication
-key = [5 5 5; 6 6 6; 3 3 3]
+    # key has to be 3 x 3 and invertible
+    key = [1 5 9; 3 77 8; 99 88 4]
 
-function setKey(x::Matrix)
-    this.key = x
-end
+    function setKey(x::Matrix)
+        global key
+        key = x
+    end
 
-function multiply(x::String)
+    function multiply(x::String)
         matrix = reshape(vcat([Int(c) for c in x], zeros(Int, 3 - length(x) % 3 == 3 ? 0 : 3 - length(x) % 3)), 3, Int(round(length(x) / 3, RoundUp)))
         enc = *(key, matrix)
         return String([Char(c) for c in enc if c != 0])
     end
 
-    export multiply, setKey
+    function decrypt(x::String)
+        matrix = reshape(vcat([Int(c) for c in x], zeros(Int, 3 - length(x) % 3 == 3 ? 0 : 3 - length(x) % 3)), 3, Int(round(length(x) / 3, RoundUp)))
+        display(key)
+        display(inv(key))
+        dec = *(inv(key), matrix)
+        return String([Char(round(c)) for c in dec if c != 0])
+    end
+
+    export multiply, setKey, decrypt
 end
